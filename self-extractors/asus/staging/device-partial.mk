c4d5774441b35b6765f69c02860f3d160b0257f9
Conley Owens <cco3@android.com>
2013-10-16 15:41:21 -0700

    Update proprietary blobs for deb
    Change-Id: I33fb2de37ccd400a67b454f9eb1994022419273a

--- self-extractors/asus/staging/device-partial.mk
+++ self-extractors/asus/staging/device-partial.mk
@@ -15,6 +15,6 @@
 # Asus blob(s) necessary for Deb hardware
 PRODUCT_COPY_FILES := \
     vendor/asus/deb/proprietary/apns-conf.xml:system/etc/apns-conf.xml:asus \
-    vendor/asus/deb/proprietary/libAKM.so:system/lib/libAKM.so:asus \
     vendor/asus/deb/proprietary/libacdbdata.so:system/lib/libacdbdata.so:asus \
+    vendor/asus/deb/proprietary/libAKM.so:system/lib/libAKM.so:asus \
 
