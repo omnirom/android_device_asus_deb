62c15306461c165a42546a5ee40c4bbbb1ad832f
havok87 <andre.sookram@gmail.com>
2014-03-27 19:24:00 -0400

    deb: use common_tablet.mk
    Change-Id: I6483e4535ea36bf601c0801b1b94448b6e4ee761

--- omni_deb.mk
+++ omni_deb.mk
@@ -24,7 +24,7 @@ $(call inherit-product, vendor/omni/config/gsm.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 # Inherit from our custom product configuration 
-$(call inherit-product, vendor/omni/config/common.mk)
+$(call inherit-product, vendor/omni/config/common_tablet.mk)
 
 PRODUCT_NAME := omni_deb
 PRODUCT_DEVICE := deb
